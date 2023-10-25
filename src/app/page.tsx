8cb374e4121eb11d4f0b0a33eb0a5fe643e842f2
VGCod3 <vktrgavryliuk@gmail.com>
2023-10-26 02:10:42 +0300

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Organizers from "@/components/Organizers";
+import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
   return (
     <main className="bg-bec-bg">
       <Organizers />
+      <Footer />
     </main>
   )
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,24 +1,26 @@
-import { play } from "@/fonts"
+import { play } from "@/fonts";
+import styles from './Footer.module.scss';
+import Image from "next/image";
 
 export default function Footer() {
-    return <footer className="w-full p-8">
-        <div className="max-w-7xl mx-auto h-full flex flex-col md:flex-row justify-between items-center gap-5">
-            <div className="socials flex gap-5">
-                <a href="https://www.facebook.com/BEST.Lviv/">
-                    <img src="/facebookWhite.svg" alt="facebook" className="w-8 md:w-auto" />
-                </a>
-                <a href="https://www.instagram.com/best_lviv/">
-                    <img src="/instagramWhite.svg" alt="instagram" className="w-8 md:w-auto" />
-                </a>
-                <a href="https://www.linkedin.com/company/bestlviv/">
-                    <img src="/linkedinWhite.svg" alt="linkedin" className="w-8 md:w-auto" />
-                </a>
-                <a href="https://t.me/bestlviv">
-                    <img src="/telegramWhite.svg" alt="telegram" className="w-8 md:w-auto" />
-                </a>
-            </div>
-            <img src="/BestLogo.svg" alt="best logo" className=' h-14 md:h-20' />
-            <p className={`${play.className} text-5xl`}>2023</p>
+    return <footer className={styles.footer}>
+        <Image width={200} height={100} src="/BestLogo.svg" alt="best logo" className='w-1/6 h-auto' />
+
+        <div className="socials flex gap-3 lg:gap-5">
+            <a href="https://www.facebook.com/BEST.Lviv/">
+                <Image width={80} height={80} src="/facebook.svg" alt="facebook" className={styles.icon} />
+            </a>
+            <a href="https://www.instagram.com/best_lviv/">
+                <Image width={80} height={80} src="/instagram.svg" alt="instagram" className={styles.icon} />
+            </a>
+            <a href="https://www.linkedin.com/company/bestlviv/">
+                <Image width={80} height={80} src="/linkedin.svg" alt="linkedin" className={styles.icon} />
+            </a>
+            <a href="https://t.me/bestlviv">
+                <Image width={80} height={80} src="/telegram.svg" alt="telegram" className={styles.icon} />
+            </a>
         </div>
+
+        <p className={`${play.className} text-3xl my-0`}>2023</p>
     </footer>
 }

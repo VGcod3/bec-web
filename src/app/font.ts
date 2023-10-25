bc7abb514b5dd423ffc061d9c9b9a149ec3542d9
Yulia Sinichak <yuliasinichak@gmail.com>
2023-10-26 00:36:23 +0300

    NEW commit AYE

--- src/app/font.ts
+++ src/app/font.ts
@@ -4,10 +4,11 @@ import {IBM_Plex_Mono} from 'next/font/google'
 export const anonymous_pro = Anonymous_Pro(
     {weight: "700", subsets: ['latin', 'cyrillic', 'latin-ext'] }
 )
+
 export const ibm_plex_mono_bold = IBM_Plex_Mono(
     {weight: ["700"], subsets: ['cyrillic-ext']}
 )
 export const ibm_plex_mono = IBM_Plex_Mono(
-    {weight: ["300"], subsets: ['cyrillic-ext']}
+    {weight: ["400"], subsets: ['cyrillic-ext']}
 )
 
--- src/components/Organizers/Organizers.tsx
+++ src/components/Organizers/Organizers.tsx
@@ -1,42 +1,40 @@
 import styles from './Organizers.module.scss';
 import {anonymous_pro} from "@/app/font";
-
 import {ibm_plex_mono} from "@/app/font";
 import {ibm_plex_mono_bold} from "@/app/font";
+
 export default function Organizers() {
     return (
-        <section className="min-h-screen flex flex-col items-start justify-center px-6 mx-auto ">
-            <div className="relative w-full hidden lg:flex justify-center my-16">
-                <h2 className='absolute -top-8 right-1/3 bg-bec-bg self-center uppercase w-fit px-1 text-5xl md:text-7xl  text-center '>Організатори</h2>
+        <section>
+            <div className="justify-center">
+                <h1 className="text-center text-5xl mt-4 lg:text-6xl mb-4">Організатори</h1>
             </div>
 
-            <h2 className='lg:hidden text-white self-center uppercase px-1 text-5xl md:text-7xl text-center mb-20'>Організатори</h2>
-
-            <div className="lg:grid grid-cols-12  gap-20 mx-auto lg:p-10">
-                <div className="col-span-12 lg:col-span-5 flex items-center md:justify-center max-w-full pb-5">
-                    <img src="/best_logo.png" alt="logo" className="max-w-full h-auto" />
+            <div className="lg:grid grid-cols-12 gap-16 pt-1 mt-1 mx-auto lg:p-10">
+                <div className="col-span-12 lg:col-span-5 flex items-center md:justify-center max-w-full pb-10">
+                    <img src="/best_logo.png" alt="logo" className="max-w-full h-auto " />
                 </div>
-                <div className="col-span-12 lg:col-span-7 lg:p-8">
-                    <h4 className={`${ibm_plex_mono_bold.className} text-left font-bold text-becOrange text-md md:text-2xl mb-4`}>МІСІЯ BEST — РОЗВИТОК СТУДЕНТІВ.</h4>
-                    <p className={`${ibm_plex_mono.className} text-md mb-5 mx-0`}>Проєкт реалізує волонтерська організація BEST Lviv, що є частиною Ради студентів технічних університетів Європи (Board of European Students of Technology).</p>
-                    <p className={`${ibm_plex_mono.className} text-md mb-5 mx-0`}>Наш осередок був сформований у 2002 році при Національному університеті "Львівська політехніка". Вже понад двадцять років BEST є дружньою спільнотою студентів, які зацікавлені в розвитку себе та суспільства загалом. На цей момент активних членів осередку налічується близько сотні.</p>
+                <div className="col-span-12 lg:col-span-7 p-8 lg:p-2">
+                    <h4 className={`${ibm_plex_mono_bold.className} text-center lg:text-left font-bold max-w-full text-becOrange text-2xl mt-1 md:text-3xl mb-8`}>МІСІЯ BEST — РОЗВИТОК СТУДЕНТІВ.</h4>
+                    <p className={`${ibm_plex_mono.className} text-center lg:text-left mt-12 mb-4 mx-0`}>Проєкт реалізує волонтерська організація BEST Lviv, що є частиною Ради студентів технічних університетів Європи (Board of European Students of Technology).</p>
+                    <p className={`${ibm_plex_mono.className} text-center lg:text-left mb-5 mx-0`}>Наш осередок був сформований у 2002 році при Національному університеті "Львівська політехніка". Вже понад двадцять років BEST є дружньою спільнотою студентів, які зацікавлені в розвитку себе та суспільства загалом. На цей момент активних членів осередку налічується близько сотні.</p>
                 </div>
             </div>
-            <p className={`${anonymous_pro.className} text-center text-2xl mx-auto`}>Усі проєкти спрямовані на допомогу студентам на початку кар’єрного росту, розвиток їхніх навичок та об’єднанню активної молоді.
+            <p className={`${anonymous_pro.className} text-center mt-8 lg:mt-1 text-2xl mx-auto`}>Усі проєкти спрямовані на допомогу студентам на початку кар’єрного росту, розвиток їхніх навичок та об’єднанню активної молоді.
             </p>
-            <p className={`${anonymous_pro.className} text-center text-2xl mx-auto`}>Ось деякі з них:</p>
+            <p className={`${anonymous_pro.className} text-center text-2xl pb-10 lg:pb-7 mx-auto`}>Ось деякі з них:</p>
 
-            <div className="grid grid-cols-12 items-center gap-12 my-6 max-w-7xl mx-auto">
-                <div className="col-span-2">
+            <div className="grid grid-cols-8 lg:grid-cols-12 items-center gap-12 my-3 lg:my-1 max-w-7xl mx-auto">
+                <div className="col-span-4 lg:col-span-2">
                     <img src="/hack.svg" alt="hack" className="mx-auto" />
                 </div>
-                <div className="col-span-4">
+                <div className="col-span-4 lg:col-span-4">
                     <img src="/bci.svg" alt="bci" className="mx-auto" />
                 </div>
-                <div className="col-span-4">
+                <div className="col-span-4 lg:col-span-2">
                     <img src="/btw.svg" alt="btw" className="mx-auto" />
                 </div>
-                <div className="col-span-2">
+                <div className="col-span-4 lg:col-span-4">
                     <img src="/ejf.svg" alt="ejf" className="mx-auto" />
                 </div>
             </div>
